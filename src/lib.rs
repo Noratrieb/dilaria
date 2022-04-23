930955590d10a4e258af1d29b6ba0a86fd3abed0
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-04-23 17:24:25 +0200

    lol

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,7 @@
-#![deny(clippy::disallowed_type)]
-
 mod ast;
 mod errors;
 
 use crate::ast::Program;
-use std::io::Write;
-
-pub use bumpalo::Bump;
-
-#[cfg(not(feature = "fxhash"))]
-#[allow(clippy::disallowed_types)]
-type HashMap<K, V> = std::collections::HashMap<K, V>;
-
-#[cfg(feature = "fxhash")]
-type HashMap<K, V> = rustc_hash::FxHashMap<K, V>;
-
-#[cfg(not(feature = "fxhash"))]
-#[allow(clippy::disallowed_types)]
-type HashSet<T> = std::collections::HashSet<T>;
-
-#[cfg(feature = "fxhash")]
-type HashSet<T> = rustc_hash::FxHashSet<T>;
 
 pub fn process_ast(program: &str, ast: &Program) {
     dbg(ast);
